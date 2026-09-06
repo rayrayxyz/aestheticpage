185ebdf7a87faa928ea0f1f51e7d9677096f70e7
rayrayxyz <sayoriono12@gmail.com>
2026-09-07 00:50:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function sayHello() {
     alert("★");
 }
+function showHome() {
+    document.getElementById("content").innerHTML = `
+        <h2>HOME ★</h2>
+        <p>Welcome to my little corner of the internet</p>
+    `;
+}
+
+function showAesthetics() {
+    document.getElementById("content").innerHTML = `
+        <h2>✧ AESTHETICS ✧</h2>
+        <p> grunge</p>
+        <p> angel</p>
+        <p> creepy</p>
+        <p> vocaloid</p>
+        <p> night</p>
+        <p> star★</p>
+        <p> Rain</p>
+    `;
+}

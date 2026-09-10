9e5d291c7c102eb6e78cbec727618bd040a7c3d6
rayrayxyz <sayoriono12@gmail.com>
2026-09-10 22:44:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function showAesthetics() {
 
         <button onclick="setTheme('grunge')">🖤 Grunge</button>
         <button onclick="setTheme('rainy')">🌧️ Rainy</button>
-        <button onclick="setTheme('monotone')">:\ Y2K</button>
-        <button onclick="setTheme('vocaloid')">💿\ Y2K</button>
+        <button onclick="setTheme('monotone')">:\ monotone</button>
+        <button onclick="setTheme('vocaloid')">💿\ vocaloid</button>
 
     `;
 }
